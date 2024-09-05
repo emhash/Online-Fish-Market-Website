525c2260abb7417db933415eb61e37d4117d72ca
unknown <md.e.h.ashiq@gmail.com>
2024-09-05 20:30:06 +0600

    update

--- market/templatetags/common.py
+++ market/templatetags/common.py
@@ -3,6 +3,11 @@ from market.models import CartItem, Order
 
 register = template.Library()
 
+
+@register.filter(name='formclass')
+def formclass(field, css_class):
+    return field.as_widget(attrs={"class": css_class})
+
 @register.filter
 def cart_data(profile):
     if profile:
